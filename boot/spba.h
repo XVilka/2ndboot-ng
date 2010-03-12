ce1f1f39c9a1c2b26e80a18ffc4c61c5d516495f
Anton Kochkov <xvilka@gmail.com>
2010-03-12 06:02:12 +0300

    small fixes

--- boot/spba.h
+++ boot/spba.h
@@ -1,3 +1,27 @@
+/*
+ *  A module for creating rebootless custom image boot support.
+ *
+ *  Copyright (C) 2010 XVilka <xvilka at gmail.com>
+ *
+ *  Inspired by 2ndboot by dimich: http://hg.ezxdev.org/2ndboot/
+ * 
+ *  This file is part of 2ndboot-ng.
+ *
+ *  2ndboot-ng is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with 2ndboot-ng.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+*/
+
 #ifndef __HW_SPBA_H__
 #define __HW_SPBA_H__
 
