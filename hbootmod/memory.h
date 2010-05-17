2121c4643a8b36a487d7a9c610ab90dd471963c1
Anton Kochkov <xvilka@gmail.com>
2010-05-17 18:48:25 +0400

    Added memory functions file

---
+++ hbootmod/memory.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   memory.h
+ * Author: xvilka
+ *
+ * Created on May 17, 2010, 4:03 PM
+ */
+
+#ifndef _MEMORY_H
+#define	_MEMORY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _MEMORY_H */
+
